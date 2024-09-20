d61b07ee11e336a5634e7f1f3cf75ed9741d10e7
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-09-19 21:15:58 -0300

    feat: create User Model

---
+++ src/models/userModel.js
@@ -0,0 +1,33 @@
+const {DataTypes} = require("sequelize");
+const sequelize = require("../config/db");
+
+const User = sequelize.define("User", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        tableName: "first_name",
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        tableName: "last_name",
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+}, {
+    tableName: "users",
+});
+
+module.exports = User;
